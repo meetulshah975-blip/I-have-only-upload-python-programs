9ac59c2e3a4d0d21c44c117d83692b007e10ef22
meetulshah975-blip <meetulshah975@gmail.com>
2025-10-17 11:14:01 +0530

    Add files via upload

---
+++ Odd Even.py
@@ -0,0 +1,5 @@
+x=int(input("Enter 1st number:"))
+if x%2 ==0:
+    print("It is even")
+else:
+        print ("It is odd")
---
+++ dictionary.py
@@ -0,0 +1,6 @@
+L={"id":[1,2,3],
+"name":["prince","vikram","mitul"],
+"age":[12,15,18]
+}
+
+print(L.values())
